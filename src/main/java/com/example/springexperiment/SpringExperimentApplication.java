ebf8899c76b5735807c8155153e1a3b520ee0518
yonding <uwrgoy7584@khu.ac.kr>
2023-08-11 21:26:06 +0900

    Initial Commit

---
+++ src/main/java/com/example/springexperiment/SpringExperimentApplication.java
@@ -0,0 +1,13 @@
+package com.example.springexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExperimentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExperimentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springexperiment/SpringExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExperimentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
